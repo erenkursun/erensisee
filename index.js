daa071f59e5c55d75c466d3149142ce598e40cca
Glitch (wooden-axiomatic-stoat) <none>
2023-03-25 08:43:16 +0000

    🍲📫 Checkpoint ./config.js:52890932/144 ./index.js:52890932/14594

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
-const { Client, GatewayIntentBits, Partials, EmbedBuilder } = require("discord.js");
+const {
+  Client,
+  GatewayIntentBits,
+  Partials,
+  EmbedBuilder,
+} = require("discord.js");
 const config = require("./config.js");
-const db = require("croxydb")
+const db = require("croxydb");
 const client = new Client({
   partials: [
     Partials.Message, // for message
@@ -33,50 +38,63 @@ const client = new Client({
 
 module.exports = client;
 
-require("./events/message.js")
-require("./events/ready.js")
-client.on('interactionCreate', async (interaction) => {
-	if (!interaction.isButton()) return;
-  let message = await interaction.channel.messages.fetch(interaction.message.id)
-  if(interaction.customId == "evet") {
-const db = require("croxydb")
-db.push(`evet_${interaction.message.id}`, interaction.user.id)
-interaction.reply({content: "Başarıyla Oyunu **Evet** Olarak Verdin!", ephemeral: true})
+require("./events/message.js");
+require("./events/ready.js");
+client.on("interactionCreate", async (interaction) => {
+  if (!interaction.isButton()) return;
+  let message = await interaction.channel.messages.fetch(
+    interaction.message.id
+  );
+  if (interaction.customId == "evet") {
+    const db = require("croxydb");
+    db.push(`evet_${interaction.message.id}`, interaction.user.id);
+    interaction.reply({
+      content: "Başarıyla Oyunu **Evet** Olarak Verdin!",
+      ephemeral: true,
+    });
 
-const evet = db.get(`evet_${interaction.message.id}`).length;
-const hayir = db.get(`hayir_${interaction.message.id}`).length;
-const aciklama = db.get(`oylama_${interaction.message.id}`)
-const embed = new EmbedBuilder()
-.setTitle("Godzilla - Oylama Sistemi!")
-.setDescription(`Oylama: **${aciklama}**\n\nEvet: **${evet}**\n\nHayır: **${hayir}**`)
-.setColor("#ff0000")
-await message.edit({embeds: [embed]})
+    const evet = db.get(`evet_${interaction.message.id}`).length;
+    const hayir = db.get(`hayir_${interaction.message.id}`).length;
+    const aciklama = db.get(`oylama_${interaction.message.id}`);
+    const embed = new EmbedBuilder()
+      .setTitle("Godzilla - Oylama Sistemi!")
+      .setDescription(
+        `Oylama: **${aciklama}**\n\nEvet: **${evet}**\n\nHayır: **${hayir}**`
+      )
+      .setColor("#ff0000");
+    await message.edit({ embeds: [embed] });
   }
 });
-client.on('interactionCreate', async (interaction) => {
-	if (!interaction.isButton()) return;
-  let message = await interaction.channel.messages.fetch(interaction.message.id)
-  if(interaction.customId == "hayır") {
-const db = require("croxydb")
-db.push(`hayir_${interaction.message.id}`, interaction.user.id)
-interaction.reply({content: "Başarıyla Oyunu **Hayır** Olarak Verdin!", ephemeral: true})
+client.on("interactionCreate", async (interaction) => {
+  if (!interaction.isButton()) return;
+  let message = await interaction.channel.messages.fetch(
+    interaction.message.id
+  );
+  if (interaction.customId == "hayır") {
+    const db = require("croxydb");
+    db.push(`hayir_${interaction.message.id}`, interaction.user.id);
+    interaction.reply({
+      content: "Başarıyla Oyunu **Hayır** Olarak Verdin!",
+      ephemeral: true,
+    });
 
-const evet = db.get(`evet_${interaction.message.id}`).length;
-const hayir = db.get(`hayir_${interaction.message.id}`).length;
-const aciklama = db.get(`oylama_${interaction.message.id}`)
-const embed = new EmbedBuilder()
-.setTitle("Godzilla - Oylama Sistemi!")
-.setDescription(`Oylama: **${aciklama}**\n\nEvet: **${evet}**\n\nHayır: **${hayir}**`)
-.setColor("#ff0000")
-await message.edit({embeds: [embed]})
+    const evet = db.get(`evet_${interaction.message.id}`).length;
+    const hayir = db.get(`hayir_${interaction.message.id}`).length;
+    const aciklama = db.get(`oylama_${interaction.message.id}`);
+    const embed = new EmbedBuilder()
+      .setTitle("Godzilla - Oylama Sistemi!")
+      .setDescription(
+        `Oylama: **${aciklama}**\n\nEvet: **${evet}**\n\nHayır: **${hayir}**`
+      )
+      .setColor("#ff0000");
+    await message.edit({ embeds: [embed] });
   }
 });
 
-client.on("messageCreate", async message => {
+client.on("messageCreate", async (message) => {
   const db = require("croxydb");
 
   if (await db.get(`afk_${message.author.id}`)) {
-   
     db.delete(`afk_${message.author.id}`);
 
     message.channel.send("Artık afk degilsin");
@@ -92,125 +110,148 @@ client.on("messageCreate", async message => {
 });
 
 client.on("messageCreate", (message) => {
-  const db = require("croxydb")
-  let kufur = db.fetch(`kufurengel_${message.guild.id}`)
-  if(!kufur) return;
-  
-  if(kufur) {
-  const kufurler = [
-    
-    "amk",
-    "piç",
-    "yarrak",
-    "oç",
-    "göt",
-    "amq",
-    "yavşak",
-    "amcık",
-    "amcı",
-    "orospu",
-    "sikim",
-    "sikeyim",
-    "aq",
-    "mk"
-       
-  ]
-  
-if(kufurler.some(alo => message.content.toLowerCase().includes(alo))) {
-message.delete()
-message.channel.send(`Hey <@${message.author.id}>, Bu Sunucuda Küfür Engel Sistemi Aktif! `)
-}
-}
-})
-client.on("messageCreate", (message) => {
-  const db = require("croxydb")
-  let reklamlar = db.fetch(`reklamengel_${message.guild.id}`)
-  if(!reklamlar) return;
-  
-  if(reklamlar) {
+  const db = require("croxydb");
+  let kufur = db.fetch(`kufurengel_${message.guild.id}`);
+  if (!kufur) return;
 
-  const linkler = [
-    
-    ".com.tr",
-    ".net",
-    ".org",
-    ".tk",
-    ".cf",
-    ".gf",
-    "https://",
-    ".gq",
-    "http://",
-    ".com",
-    ".gg",
-    ".porn",
-    ".edu"
-       
-  ]
-  
-if(linkler.some(alo => message.content.toLowerCase().includes(alo))) {
-message.delete()
-message.channel.send(`Hey <@${message.author.id}>, Bu Sunucuda Reklam Engel Sistemi Aktif! `)
-}
-}
-})
+  if (kufur) {
+    const kufurler = [
+      "amk",
+      "piç",
+      "yarrak",
+      "oç",
+      "göt",
+      "amq",
+      "yavşak",
+      "amcık",
+      "amcı",
+      "orospu",
+      "sikim",
+      "sikeyim",
+      "aq",
+      "mk",
+    ];
 
-client.on('guildMemberAdd', async member => {
-  let sayac = db.fetch(`sayac_${member.guild.id}`)
-  let kalan = sayac.sayi - member.guild.memberCount || '?'
-  if(!kalan) return;
-  if(!sayac) return;
-  
-  client.channels.cache.get(sayac.kanal).send("<a:duyur:1084953665049219112> Hoşgeldin **"+member.user.username+"** Seninle Beraber `"+member.guild.memberCount+"` Kişi Olduk, `"+sayac.sayi+"` Kişi Olmamıza Son `"+kalan+"` Kişi Kaldı! <a:psoprime_emoji:1084585759195021422>")
-  
-});
-client.on('guildMemberRemove', async member => {
-  
-  let sayac = db.fetch(`sayac_${member.guild.id}`)
-  let kalan = sayac.sayi - member.guild.memberCount
-  if(!sayac) return;
-  
-  client.channels.cache.get(sayac.kanal).send("<:cikis:1084586075017719898> Görüşürüz **"+member.user.username+"** Senin Yüzünden `"+member.guild.memberCount+"` Kişi Olduk! <a:psoprime_emoji:1084585759195021422>")
-  
-});
-
-client.on('guildMemberAdd', async member => {
-  
-  let otorol = db.fetch(`otorol_${member.guild.id}`)
-  if(!otorol) return;
-  
-  client.channels.cache.get(otorol.kanal).send("<:giris:1084586043870806036> **"+member.user.tag+"** Kullanıcı Katıldı! Gerekli Rolleri Verdim. <a:psoprime_emoji:1084585759195021422>")
-  member.roles.add(otorol.rol).catch(() => {})
-  
-});
-
-
-client.on("messageCreate", (message) => {
-  
-  let saas = db.fetch(`saas_${message.guild.id}`)
-  if(!saas) return;
-  
-  if(saas) {
-  
-  let selaamlar = message.content.toLowerCase()  
-if(selaamlar === 'sa' || selaamlar === 'slm' || selaamlar === 'sea' || selaamlar === ' selamünaleyküm' || selaamlar === 'Selamün Aleyküm' || selaamlar === 'selam'){
-
-message.channel.send(`<@${message.author.id}> Aleykümselam, Hoşgeldin ☺️`)
-}
-}
-})
-
-client.on("messageCreate", async message => {
-  
-  const cmd = db.fetch(`otocevap_${message.content}`)
-  if(!cmd) return;
-  
-  if(cmd) {
-    message.reply({ content: `${cmd.answer}` })
+    if (kufurler.some((alo) => message.content.toLowerCase().includes(alo))) {
+      message.delete();
+      message.channel.send(
+        `Hey <@${message.author.id}>, Bu Sunucuda Küfür Engel Sistemi Aktif! `
+      );
+    }
   }
-  
 });
-client.on("guildMemberAdd", async member => {
-  let ototag = db.get(`ototag_${member.guild.id}`);;
-  if (ototag) return member.setNickname(`${ototag} ${member.user.username}`)
-})
-client.login(process.env.token)
+client.on("messageCreate", (message) => {
+  const db = require("croxydb");
+  let reklamlar = db.fetch(`reklamengel_${message.guild.id}`);
+  if (!reklamlar) return;
+
+  if (reklamlar) {
+    const linkler = [
+      ".com.tr",
+      ".net",
+      ".org",
+      ".tk",
+      ".cf",
+      ".gf",
+      "https://",
+      ".gq",
+      "http://",
+      ".com",
+      ".gg",
+      ".porn",
+      ".edu",
+    ];
+
+    if (linkler.some((alo) => message.content.toLowerCase().includes(alo))) {
+      message.delete();
+      message.channel.send(
+        `Hey <@${message.author.id}>, Bu Sunucuda Reklam Engel Sistemi Aktif! `
+      );
+    }
+  }
+});
+
+client.on("guildMemberAdd", async (member) => {
+  let sayac = db.fetch(`sayac_${member.guild.id}`);
+  let kalan = sayac.sayi - member.guild.memberCount || "?";
+  if (!kalan) return;
+  if (!sayac) return;
+
+  client.channels.cache
+    .get(sayac.kanal)
+    .send(
+      "<a:duyur:1084953665049219112> Hoşgeldin **" +
+        member.user.username +
+        "** Seninle Beraber `" +
+        member.guild.memberCount +
+        "` Kişi Olduk, `" +
+        sayac.sayi +
+        "` Kişi Olmamıza Son `" +
+        kalan +
+        "` Kişi Kaldı! <a:psoprime_emoji:1084585759195021422>"
+    );
+});
+client.on("guildMemberRemove", async (member) => {
+  let sayac = db.fetch(`sayac_${member.guild.id}`);
+  let kalan = sayac.sayi - member.guild.memberCount;
+  if (!sayac) return;
+
+  client.channels.cache
+    .get(sayac.kanal)
+    .send(
+      "<:cikis:1084586075017719898> Görüşürüz **" +
+        member.user.username +
+        "** Senin Yüzünden `" +
+        member.guild.memberCount +
+        "` Kişi Olduk! <a:psoprime_emoji:1084585759195021422>"
+    );
+});
+
+client.on("guildMemberAdd", async (member) => {
+  let otorol = db.fetch(`otorol_${member.guild.id}`);
+  if (!otorol) return;
+
+  client.channels.cache
+    .get(otorol.kanal)
+    .send(
+      "<:giris:1084586043870806036> **" +
+        member.user.tag +
+        "** Kullanıcı Katıldı! Gerekli Rolleri Verdim. <a:psoprime_emoji:1084585759195021422>"
+    );
+  member.roles.add(otorol.rol).catch(() => {});
+});
+
+client.on("messageCreate", (message) => {
+  let saas = db.fetch(`saas_${message.guild.id}`);
+  if (!saas) return;
+
+  if (saas) {
+    let selaamlar = message.content.toLowerCase();
+    if (
+      selaamlar === "sa" ||
+      selaamlar === "slm" ||
+      selaamlar === "sea" ||
+      selaamlar === " selamünaleyküm" ||
+      selaamlar === "Selamün Aleyküm" ||
+      selaamlar === "selam"
+    ) {
+      message.channel.send(
+        `<@${message.author.id}> Aleykümselam, Hoşgeldin ☺️`
+      );
+    }
+  }
+});
+
+client.on("messageCreate", async (message) => {
+  const cmd = db.fetch(`otocevap_${message.content}`);
+  if (!cmd) return;
+
+  if (cmd) {
+    message.reply({ content: `${cmd.answer}` });
+  }
+});
+client.on("guildMemberAdd", async (member) => {
+  let ototag = db.get(`ototag_${member.guild.id}`);
+  if (ototag) return member.setNickname(`${ototag} ${member.user.username}`);
+});
+client.login(process.env.token);
